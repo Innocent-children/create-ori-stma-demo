8bf516eef615230f44a80c701b0e3ae11767a790
Innocent-children <55626758+Innocent-children@users.noreply.github.com>
2025-07-26 10:52:19 +0800

    Add main application file

---
+++ src/main/java/com/stma/MainApplication.java
@@ -0,0 +1,11 @@
+package com.stma;
+
+/**
+ * Main application class
+ */
+public class MainApplication {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello, GitHub API demo!");
+    }
+}
